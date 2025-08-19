944b1053afa318abfd22742b7c78bfafc7499054
ElementalMP4 <sebahudl@gmail.com>
2025-08-19 11:05:47 +0100

    Handy new plugin

---
+++ client/plugins/reference/main.lua
@@ -0,0 +1,35 @@
+Reference = {}
+
+local state_file = "refs.ini"
+local refs = {}
+
+local function load_refs()
+    local f = io.open(state_file, "r")
+    if not f then return end
+    for line in f:lines() do
+        local key, val = line:match("^(.-)=(%d+)$")
+        if key and val then
+            refs[key] = tonumber(val)
+        end
+    end
+    f:close()
+end
+
+local function save_refs()
+    local f = io.open(state_file, "w")
+    if not f then return end
+    for k, v in pairs(refs) do
+        f:write(k .. "=" .. v .. "\n")
+    end
+    f:close()
+end
+
+function Reference.ref(target)
+    refs[target] = (refs[target] or 0) + 1
+    save_refs()
+    return target .. "-" .. refs[target]
+end
+
+load_refs()
+
+return Reference
